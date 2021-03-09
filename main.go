7d36309be99ff0f27b5e2338534f09fceb14b077
G.Sterczewski <sterczewski.dev@protonmail.ch>
2021-03-09 12:10:39 +0100

    [dev] basic functionality draft

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/fcc-microservices/router"
 	"github.com/fcc-microservices/services"
@@ -12,9 +14,25 @@ import (
 const port = 8080
 const apiPath = "/api"
 
+func getDateStringFromURI(req *http.Request, path string) string {
+	input := req.URL.Path[len(path):]
+	if len(input) == 0 {
+		return ""
+	}
+	inputSlice := strings.Split(input, "/")
+	return inputSlice[0]
+
+}
 func handleTimestamp(rw http.ResponseWriter, req *http.Request) {
 	ts := services.Timestamp{}
-	ts.Run(rw)
+	timestamp, err := ts.Parse(getDateStringFromURI(req, "/api/timestamp/"))
+	if err != nil {
+		fmt.Fprintf(rw, "Error : %s", err)
+	}
+	encoder := json.NewEncoder(rw)
+	encoder.Encode(&timestamp)
+	fmt.Fprint(rw)
+
 }
 
 func main() {
--- services/timestamp.go
+++ services/timestamp.go
@@ -3,10 +3,138 @@ package services
 import (
 	"fmt"
 	"io"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
 )
+
+func filterOutWhitespaces(arr []string) []string {
+	filtered := []string{}
+	for _, v := range arr {
+		if len(v) > 0 {
+			filtered = append(filtered, v)
+		}
+	}
+	return filtered
+}
+
+type parsedDate struct {
+	day    int
+	month  int
+	year   int
+	hour   int
+	minute int
+	second int
+}
+
+func newParsedDate() parsedDate {
+	return parsedDate{0, 0, 0, 0, 0, 0}
+}
+
+func (pd *parsedDate) SetYear(year int) {
+	pd.year = year
+}
+func (pd *parsedDate) SetMonth(month int) {
+	pd.month = month
+}
+func (pd *parsedDate) SetDay(day int) {
+	pd.day = day
+}
+
+func parseDateString(ds []int) parsedDate {
+	pd := newParsedDate()
+	dsLen := len(ds)
+	switch dsLen {
+	case 1:
+		pd.SetYear(ds[0])
+	case 2:
+		pd.SetYear(ds[0])
+		pd.SetMonth(ds[1])
+	case 3:
+		pd.SetYear(ds[0])
+		pd.SetMonth(ds[1])
+		pd.SetDay(ds[2])
+	}
+	return pd
+}
+
+// DateError - custom error type for handling invalid dates
+type DateError struct{}
+
+func (de DateError) Error() string {
+	return fmt.Sprint("Invalid date")
+}
+
+//TimestampResponse - struct representing response type for the client
+type TimestampResponse struct {
+	Unix int64  `json:"unix"`
+	Utc  string `json:"utc"`
+}
 
 //Timestamp -
 type Timestamp struct{}
+
+func (ts Timestamp) isValidUnix(u string) bool {
+
+	if strings.HasSuffix(u, "-") {
+		return false
+	}
+	converted, err := strconv.ParseInt(u, 10, 64)
+	if err != nil {
+		return false
+	}
+	if converted < 0 {
+		return false
+	}
+	return true
+}
+
+func (ts Timestamp) fromUnix(u int64) TimestampResponse {
+	return newTimestampResponse(u)
+}
+
+func (ts Timestamp) fromDateString(ds string) (TimestampResponse, error) {
+	dsElements := filterOutWhitespaces(strings.Split(ds, "-"))
+	convertedElements := []int{}
+	for _, element := range dsElements {
+		converted, err := strconv.ParseInt(element, 10, 64)
+		if err != nil {
+			return TimestampResponse{}, DateError{}
+		}
+		convertedElements = append(convertedElements, int(converted))
+	}
+	pd := parseDateString(convertedElements)
+	loc, _ := time.LoadLocation("Local")
+	date := time.Date(pd.year, time.Month(pd.month), pd.day, pd.hour, pd.minute, pd.second, 0, loc)
+	return newTimestampResponse(date.Unix()), nil
+
+}
+
+//Parse - takes date string as an input and decide which constructor to use to create TimestampResponse
+func (ts Timestamp) Parse(ds string) (TimestampResponse, error) {
+	if len(ds) == 0 {
+		return ts.fromUnix(time.Now().Unix()), nil
+	}
+
+	dsElements := filterOutWhitespaces(strings.SplitAfter(ds, "-"))
+
+	if len(dsElements) == 1 && ts.isValidUnix(dsElements[0]) {
+		converted, err := strconv.ParseInt(dsElements[0], 10, 64)
+		if err != nil {
+			return TimestampResponse{}, DateError{}
+		}
+		return ts.fromUnix(converted), nil
+	}
+	return ts.fromDateString(ds)
+
+}
+
+func newTimestampResponse(u int64) TimestampResponse {
+	utc := time.Unix(u, 0).Format(http.TimeFormat)
+	return TimestampResponse{u, utc}
+
+}
 
 //Run - main function for the service
 func (ts Timestamp) Run(w io.Writer) {
